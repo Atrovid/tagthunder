130aada440648069a0f5f1050a6b2dda935b3484
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-10-26 17:25:57 +0200

    api add main

---
+++ tagthunder/api/main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+import api.routers as routers
+from api.configurations.api import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    docs_url=settings.docs_url,
+    openapi_tags=routers.tags,
+)
+
+for sub_router in routers.routes:
+    app.include_router(sub_router)
+
+
+def run():
+    uvicorn.run("tagthunder.api.main:app", reload=True)
