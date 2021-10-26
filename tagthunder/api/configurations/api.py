21f8754621fcc6ba5ee8952c145a1884ce440cb9
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-10-26 17:24:35 +0200

    api conf: add settings file

---
+++ tagthunder/api/configurations/api.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+import pydantic.networks as pn
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "TagThunder API"
+    admin_email: pn.EmailStr = "francois.ledoyen@unicaen.fr"
+    # node.js app allows to build augmented HTML
+    crawler_address: pn.HttpUrl = "http://127.0.0.1:8080/"
+    host: str
+    port: Optional[int]
+    docs_url: str = "/"
+
+
+class LocalSettings(Settings):
+    host = "http://127.0.0.1"
+    port = 8000
+
+
+class AccessmanSettings(Settings):
+    host = "https://accessman.greyc.fr/api"
+
+
+settings = LocalSettings()
