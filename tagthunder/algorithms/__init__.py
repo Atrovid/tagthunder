cf5f27c7016c28d18a82d77abdb2bf730744f2f3
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-10-27 22:42:48 +0200

    Add spatialization algorithms dir

--- tagthunder/algorithms/__init__.py
+++ tagthunder/algorithms/__init__.py
@@ -1,3 +1,4 @@
 import algorithms.extraction as extraction
 import algorithms.cleaning as cleaning
 import algorithms.segmentation as segmentation
+import algorithms.spatialization as spatialization
