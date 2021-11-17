eff880b269e87f0a26b45ed1ed25b5d2abe8b056
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-11-17 15:21:52 +0100

    Update visu : method to show init and final clustering

--- tagthunder/algorithms/segmentation/utils/visualisation.py
+++ tagthunder/algorithms/segmentation/utils/visualisation.py
@@ -5,34 +5,65 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 
 from algorithms.models.web_elements import BoundingBox
+from algorithms.segmentation.utils.features_extractors import Features
 
 
 class PlotClustering:
-    def __init__(self, title=""):
-        self.fig, self.ax = plt.subplots(1)
-        self.ax.set_title = title
-        self.ax.xaxis.tick_top()
-        self.ax.invert_yaxis()
+    def __init__(self, **kwargs):
 
-    def bboxes(self, bboxes: List[BoundingBox], labels=None, **kwargs):
+        self.fig, self.axes = plt.subplots(kwargs.pop("nrows", 1), 1)
+
+        if not isinstance(self.axes, np.ndarray):
+            self.axes = np.array([self.axes])
+
+        gridspec = self.axes[0].get_subplotspec().get_gridspec()
+        self.subfigs = [self.fig.add_subfigure(gs) for gs in gridspec]
+
+    @classmethod
+    def bboxes(cls, ax, bboxes: List[BoundingBox], labels=None, **kwargs):
 
         if labels is None:
             labels = np.ones(len(bboxes))
 
         for bbox, l in zip(bboxes, labels):
-            coords = np.array(bbox)
+            coords = bbox.to_numpy()
             color = plt.cm.tab10(l)
             patch = Polygon(coords, fc="none", ec=color, **kwargs)
-            self.ax.add_patch(patch)
-            self.ax.scatter(coords[:, 0], coords[:, 1], c="none")
+            ax.add_patch(patch)
+            ax.scatter(coords[:, 0], coords[:, 1], c="none")
 
+    def population(self, ax, bboxes, labels=None):
+        self.bboxes(ax, bboxes, labels)
+        return ax
+
+    def centers(self, ax, bboxes, labels=None):
+        self.bboxes(ax, bboxes, labels, hatch="//", ls="--")
         return self
 
-    def population(self, bboxes, labels):
-        return self.bboxes(bboxes, labels)
+    def clustering_plt(self, row_id, title, population: Features, init_centers, centers, labels):
 
-    def centers(self, bboxes, labels):
-        return self.bboxes(bboxes, labels, hatch="//", ls="--")
+        fig = self.subfigs[row_id]
+        fig.suptitle(title, fontweight='semibold')
+
+        k = len(centers)
+        bboxes = population.bboxes
+        centers_labels = np.arange(k)
+
+        axes = fig.subplots(nrows=1, ncols=2)
+        for ax in axes.reshape(-1):
+            ax.invert_yaxis()
+            ax.xaxis.tick_top()
+
+        ax = axes[0]
+        ax.set_title("Init")
+        self.population(ax, bboxes)
+        self.centers(ax, init_centers, centers_labels)
+
+        ax = axes[1]
+        ax.set_title("Results")
+        self.population(ax, bboxes, labels)
+        self.centers(ax, centers, centers_labels)
+        return self
 
     def show(self):
         plt.show()
