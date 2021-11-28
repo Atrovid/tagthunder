5708037d8fde3dd32070cf9688bee2b9c14035de
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-11-28 18:54:08 +0100

    API allows cors origin

--- tagthunder/api/main.py
+++ tagthunder/api/main.py
@@ -1,3 +1,4 @@
+import fastapi.middleware.cors
 import uvicorn
 from fastapi import FastAPI
 
@@ -8,6 +9,14 @@ app = FastAPI(
     title=settings.app_name,
     docs_url=settings.docs_url,
     openapi_tags=routers.tags,
+)
+
+app.add_middleware(
+    fastapi.middleware.cors.CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 for sub_router in routers.routes:
