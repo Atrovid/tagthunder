6d66ab7647bc0035f1f84aeb86b6985caf12621e
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-11-29 13:32:44 +0100

    Add google translate to detect language (it is efficient on short text)

--- tagthunder/pipeline/blocks/vocalization/_utils.py
+++ tagthunder/pipeline/blocks/vocalization/_utils.py
@@ -2,9 +2,11 @@ import io
 from typing import List, Dict
 import pycld2
 import langdetect
+import googletrans
 
 
 class LanguageDetector:
+    GOOGLE_TRANSLATOR = googletrans.Translator(service_urls=["translate.google.com"])
 
     @classmethod
     def cld2(cls, text) -> List[Dict[str, str]]:
@@ -19,3 +21,6 @@ class LanguageDetector:
         langdetect.DetectorFactory.seed = 0
         yield text, langdetect.detect(text)
 
+    @classmethod
+    def google_transalte(cls, text) -> List[Dict[str, str]]:
+        yield text, cls.GOOGLE_TRANSLATOR.detect(text).lang
--- tagthunder/pipeline/blocks/vocalization/google_tts.py
+++ tagthunder/pipeline/blocks/vocalization/google_tts.py
@@ -1,3 +1,5 @@
+from typing import Callable, Dict
+
 from gtts import gTTS
 import io
 
@@ -7,13 +9,17 @@ from pipeline.models.responses import Zone, Keywords, Keyword
 
 
 class GoogleTTS(AbstractVocalizationBlock):
+    LANG_DETECTORS: Dict[str, Callable] = {
+        "langdetect": LanguageDetector.langdetect,
+        "google_translate": LanguageDetector.google_transalte
+    }
 
     def __call__(self, keywords: Keywords, **kwargs) -> io.BytesIO:
         fp = io.BytesIO()
 
         for k in keywords:
-            for text, lang in LanguageDetector.langdetect(k.text):
-                tts = gTTS(text=text, lang=lang, tld="com")
+            for text, lang in self.LANG_DETECTORS['google_translate'](k.text):
+                tts = gTTS(text=text, lang=lang, tld="com", slow=False)
                 tts.write_to_fp(fp)
         fp.seek(0)
 
