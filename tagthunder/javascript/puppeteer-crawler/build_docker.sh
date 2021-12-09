a4fcedb8e4eb32f6f33550e268e1a090bc3a71bc
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-12-09 21:49:38 +0100

    Update docker build and run

--- tagthunder/javascript/puppeteer-crawler/build_docker.sh
+++ tagthunder/javascript/puppeteer-crawler/build_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+DIR="$(dirname "$(realpath "$0")")"
 
+cd ../html-augmentation/ && npm pack && mv *.tgz $DIR/node_modules && cd -
+npm install
 docker build . -t tagthunder-crawler 
--- tagthunder/javascript/puppeteer-crawler/run_docker.sh
+++ tagthunder/javascript/puppeteer-crawler/run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -p 8080:8080 tagthunder-crawler
+docker run -t -i -p 8080:8080 tagthunder-crawler
