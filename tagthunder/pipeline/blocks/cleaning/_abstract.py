b33b7f2bab70f63e16f0bb698047f429ef650113
François Ledoyen <francois.ledoyen@unicaen.fr>
2021-12-10 18:40:46 +0100

    Correct vision based is_empty

--- tagthunder/pipeline/blocks/cleaning/_abstract.py
+++ tagthunder/pipeline/blocks/cleaning/_abstract.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
 
+import bs4
+
 from pipeline.models.responses import HTMLPP, HTMLP
 from pipeline.models.web_elements import HTMLPTag, HTMLPPTag
 
 
 class AbstractCleaningBlock(ABC):
+    NON_CONTAINER_TAGS = ["img"]
 
     @abstractmethod
     def __call__(self, *, htmlp: HTMLP) -> HTMLPP:
@@ -12,9 +15,30 @@ class AbstractCleaningBlock(ABC):
 
     @classmethod
     @abstractmethod
-    def _is_usable_tag(cls, tag: HTMLPTag):
+    def _is_usable_tag(cls, tag: HTMLPTag) -> bool:
         ...
 
     @classmethod
-    def mark_as_usable_tag(cls, tag: HTMLPTag, value: bool):
-        tag.attrs[HTMLPPTag.IS_USABLE_HTML_ATTR_KEY] = str(value)
+    def mark_as_usable_tag(cls, tag: HTMLPTag):
+        tag.attrs[HTMLPPTag.IS_USABLE_HTML_ATTR_KEY] = str(cls._is_usable_tag(tag)).lower()
+
+    @classmethod
+    def contains_comment(cls, htmlp: HTMLP):
+        return htmlp.contains_comments(recursive=True)
+
+    @classmethod
+    def remove_comments(cls, htmlp: HTMLP):
+        for comment in htmlp.get_comments(True):
+            comment.extract()
+
+    @classmethod
+    def is_empty_tag(cls, tag: HTMLPTag) -> bool:
+        return all((
+            tag.name not in cls.NON_CONTAINER_TAGS,
+            not list(tag.text.strip()),
+            all([cls.is_empty_tag(t) and t.name not in cls.NON_CONTAINER_TAGS for t in tag()])
+        ))
+
+    @classmethod
+    def is_flat_tag(cls, tag: HTMLPTag) -> bool:
+        return not tag.bbox.is_visible
--- tagthunder/pipeline/blocks/cleaning/vision_based.py
+++ tagthunder/pipeline/blocks/cleaning/vision_based.py
@@ -1,6 +1,4 @@
-from typing import List, Callable, Iterable
-
-import bs4
+from typing import Callable, Iterable
 
 from pipeline.blocks.cleaning._abstract import AbstractCleaningBlock
 from pipeline.models.responses import HTMLPP, HTMLP
@@ -10,52 +8,32 @@ from pipeline.models.web_elements import HTMLPTag
 class VisionBased(AbstractCleaningBlock):
     USELESS_TAGS = ["title", "link", "script", "noscript", "style", "doctype", "head", "base", "command",
                     "meta", "br"]
-    NON_CONTAINER_TAGS = ["img"]
 
     def __call__(self, htmlp: HTMLP) -> HTMLPP:
-        for tag in htmlp.find_all(True):
+        self.remove_comments(htmlp)
+        for tag in list(htmlp())[::-1]:
             if any([c(tag) for c in self.remove_conditions]):
-                tag.decompose()
+                tag.extract()
             else:
-                self.mark_as_usable_tag(tag, self._is_usable_tag(tag))
+                self.mark_as_usable_tag(tag)
 
-        return HTMLPP(htmlp)
+        return HTMLPP(str(htmlp))
 
     @classmethod
     def _is_usable_tag(cls, tag: HTMLPTag):
-        return (tag.bbox.is_usable
-                and not any([
-                    tag.styles.get("display") == "none",
-                    tag.styles.get("visiblity") == "hidden",
-                    tag.styles.get("hidden") == "true",
-                ])
-                )
+        return all([
+            tag.styles.get("display") != "none",
+            tag.styles.get("visibility") != "hidden",
+        ])
 
     @property
     def remove_conditions(self) -> Iterable[Callable]:
         return (
-            self.is_comments,
             self.is_useless_tag,
             self.is_empty_tag,
             self.is_flat_tag
         )
 
     @classmethod
-    def is_comments(cls, tag: HTMLPTag) -> bool:
-        return isinstance(tag.text, bs4.Comment)
-
-    @classmethod
     def is_useless_tag(cls, tag: HTMLPTag) -> bool:
         return tag.name in cls.USELESS_TAGS
-
-    @classmethod
-    def is_empty_tag(cls, tag: HTMLPTag) -> bool:
-        return (
-                tag.name not in cls.NON_CONTAINER_TAGS
-                and not list(tag.strings)
-                and not tag.find_all(lambda sub_tag: sub_tag.name in cls.NON_CONTAINER_TAGS)
-        )
-
-    @classmethod
-    def is_flat_tag(cls, tag: HTMLPTag) -> bool:
-        return tag.bbox.is_usable
