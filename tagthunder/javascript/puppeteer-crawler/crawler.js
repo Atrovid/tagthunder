6705722cd43bfd5f57a04f914c53f607c349056b
François Ledoyen <francois.ledoyen@unicaen.fr>
2024-02-22 16:54:29 +0100

    fix: inject computed-style-to-inline-style v3 and not latest to page

--- tagthunder/javascript/puppeteer-crawler/crawler.js
+++ tagthunder/javascript/puppeteer-crawler/crawler.js
@@ -1,77 +1,82 @@
-"use strict"
+"use strict";
 
-const puppeteer = require('puppeteer');
-const htmlAugmentation = require('html-augmentation');
-const getXPath = require('get-xpath');
+const puppeteer = require("puppeteer");
+const htmlAugmentation = require("html-augmentation");
+const getXPath = require("get-xpath");
 const computedStyleToInlineStyle = require("computed-style-to-inline-style");
-const config = require('./config');
+const config = require("./config");
 
+async function init() {
+  let browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      `--proxy-server=${config.PROXY}`,
+      `--disable-extensions-except=${config.cookieIgnorePath}`,
+      `--load-extension=${config.cookieIgnorePath}`,
+    ],
 
-async function init(){
-    let browser = await puppeteer.launch({
-        headless: false,
-        args: [
-            '--no-sandbox', 
-            '--disable-setuid-sandbox',
-            `--proxy-server=${config.PROXY}`,
-            `--disable-extensions-except=${config.cookieIgnorePath}`,
-            `--load-extension=${config.cookieIgnorePath}`,
-        ],
+    defaultViewport: null,
+  });
 
-        defaultViewport: null
-    });
-
-    console.log("[*] Browser loaded");
-    return browser;
+  console.log("[*] Browser loaded");
+  return browser;
 }
 
-async function goto(browser, url, w, h){
-    let page = await browser.newPage();
-    await page.setViewport({width: w, height: h});
-    await page.setCacheEnabled(false);
-    await page.goto(url, {waitUntil: "domcontentloaded"});
-    console.log(`[*] ${url} opened`);
-    return page;
+async function goto(browser, url, w, h) {
+  let page = await browser.newPage();
+  await page.setViewport({ width: w, height: h });
+  await page.setCacheEnabled(false);
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  console.log(`[*] ${url} opened`);
+  return page;
 }
 
-async function processPage(page, styles){
-    await addStyles(page, styles);
-    await addBoundingBox(page)
-    await addXPath(page)
-    console.log("[*] page is augmented")
+async function processPage(page, styles) {
+  await addStyles(page, styles);
+  await addBoundingBox(page);
+  await addXPath(page);
+  console.log("[*] page is augmented");
 }
 
-async function getBody(page){
-    let bodyHTML = await page.evaluate(() => window.document.body.outerHTML);
-    return bodyHTML.replace(/\s+/g, ' ').trim();
+async function getBody(page) {
+  let bodyHTML = await page.evaluate(() => window.document.body.outerHTML);
+  return bodyHTML.replace(/\s+/g, " ").trim();
 }
 
-async function addStyles(page, styles){
-    await page.addScriptTag({ url : "https://unpkg.com/computed-style-to-inline-style"})
-    await page.evaluateHandle((styles) => {
-        let options = {
-            recursive: true,
-        }
-        if (!!styles.length){options.properties = styles}
+async function addStyles(page, styles) {
+  await page.addScriptTag({
+    url: "https://unpkg.com/computed-style-to-inline-style@3.0.0",
+  });
+  await page.evaluateHandle((styles) => {
+    let options = {
+      recursive: true,
+    };
+    if (!!styles.length) options.properties = styles;
 
-        computedStyleToInlineStyle(document.body, options)
-    }, styles)
+    computedStyleToInlineStyle(document.body, options);
+  }, styles);
 }
-async function addBoundingBox(page){
-    await page.addScriptTag({content: htmlAugmentation.addBoundingBox.toString()})
-    await page.evaluateHandle(() => {
-        addBoundingBox(document.querySelector("body"));
-        Array.from(document.querySelectorAll("body *")).forEach(addBoundingBox);
-    })
+async function addBoundingBox(page) {
+  await page.addScriptTag({
+    content: htmlAugmentation.addBoundingBox.toString(),
+  });
+  await page.evaluateHandle(() => {
+    addBoundingBox(document.querySelector("body"));
+    Array.from(document.querySelectorAll("body *")).forEach(addBoundingBox);
+  });
 }
 
-async function addXPath(page){
-    await page.addScriptTag({content: `const getXPath = ${getXPath.toString()}`})
-    await page.addScriptTag({content: htmlAugmentation.addXPath.toString()})
-    await page.evaluateHandle(() => {
-        addXPath(document.querySelector("body"));
-        Array.from(document.querySelectorAll("body *")).forEach(addXPath);
-    })
+async function addXPath(page) {
+  await page.addScriptTag({
+    content: `const getXPath = ${getXPath.toString()}`,
+  });
+  await page.addScriptTag({ content: htmlAugmentation.addXPath.toString() });
+  await page.evaluateHandle(() => {
+    addXPath(document.querySelector("body"));
+    Array.from(document.querySelectorAll("body *")).forEach(addXPath);
+  });
 }
 
 exports.init = init;
